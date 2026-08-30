4a80b2783e3378adf52a618483068cbb7d4add22
Julia Schwartz <julia.schwartz@st-one.io>
2026-08-30 11:27:38 -0300

    Remove deprecated mongoose connection options from config

--- config/config.js
+++ config/config.js
@@ -5,8 +5,6 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      // useCreateIndex: true,
-      // useFindAndModify: false
     });
     console.log('MongoDB connected');
   } catch (error) {
